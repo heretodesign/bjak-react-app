a3d4aa7968acb2a8cef70390e2b661470c960fa3
heretodesign <senwelo.works@gmail.com>
2019-08-17 15:50:58 +0800

    mapping out the array response

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import { Button, table, thead, tbody, columns, column} from "react-bulma-components/full"
+import { Columns, Column } from "react-bulma-components/full"
 import 'react-bulma-components/dist/react-bulma-components.min.css'
 
 class ListPage extends React.Component {
@@ -9,71 +9,71 @@ class ListPage extends React.Component {
     movies: [],
   }
 
-  componentDidMount () {
-  axios.get('https://cdn-discover.hooq.tv/v1.2/discover/feed?region=ID&page=1&perPage=20').then(response => {
-    // console.log(response.data.data[0]);
-    let words = [...response.data.data];
-    let finalData = words.filter(word => word.type === "Multi-Title-Manual-Curation");
-    console.log(finalData[0]);
-    this.setState({
-      movies: finalData
+  componentDidMount() {
+    axios.get('https://cdn-discover.hooq.tv/v1.2/discover/feed?region=ID&page=1&perPage=20')
+      .then(response => {
+        // console.log(response.data.data);
+        let titles = [...response.data.data]; //copy the res into a new array called title using spread
+        let curated = titles.filter(word => word.type === "Multi-Title-Manual-Curation"); //use filter to filter for specific type
+        console.log(curated);
+        // console.log(curated[0].data[0].images[0].url);
+        this.setState({
+            movies: curated
+        })
     })
-  })
-  .catch(error => {
-    console.log('ERROR: ', error)
-  })
+    .catch(error => {
+        console.log('ERROR: ', error)
+    })
+  }
+​
+    render() {
+        return (
+            <section className="section">
+                <div className="container notification is-primary has-text-centered" style={{ overflowX: "scroll" }}>
+                    <h1 className="title is-1">Cool Stuff To Watch</h1>
+                    {
+                        this.state.movies.map(movie =>
+                            <>
+                                <div className="columns has-text-centered">
+                                    <div className="column">
+                                        <h1 class="title is-3">{movie.row_name}</h1>
+​
+                                    </div>
+                                </div>
+                                <div className="columns has-text-centered">
+                                    {
+                                        movie.data.map(show =>
+                                            <div className="column">
+                                                {
+                                                    show.images
+                                                        .filter(image => image.type === "POSTER")
+                                                        .map(image =>
+                                                            <div className="card">
+                                                                <div className="card-image">
+                                                                    <figure className="image is-4by3">
+                                                                        <img
+                                                                            src={image.url}
+                                                                            alt="Placeholder image"
+                                                                        />
+                                                                    </figure>
+                                                                </div>
+                                                                <div className="card-content">
+                                                                    <p className="heading is-7">{show.title}</p>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                }
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                            </>
+                        )
+                    }
+                </div>
+            </section >
+        );
+    }
 }
 
-  addComment = (taskId) => {
-    this.props.history.push(`/pages/detail/${taskId}`)
-  }
-
-
-  render() {
-
-      return (
-        <div>
-          <section className="section is-paddingless-horizontal">
-              <div className="container grid is-large notification">
-                  <div className="firstsection">
-                      <h1 className="title is-3">Att: Upcoming Events and Programmes</h1>
-                      <div className="content">
-                        <div className="columns">
-                          <div className="column" id="tablelisttask">
-                            <table className="table is-mobile">
-                              <thead>
-                                <tr>
-                                  <th><abbr title="image" className="is-3">Row Name</abbr></th>
-                                  <th><abbr title="title">Image</abbr></th>
-                                  <th><abbr title="date">Title</abbr></th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                              {this.state.movies.map((movie) => (
-                                <tr className="key={task.id}">
-                                  <td>{ movie.row_name }</td>
-                                  <Link to={`/pages/viewpage/${movie.row_id}`}>
-                                    <figure className="image is-200x200">
-                                      <td><img width="200" height="200" src={ movie.data.images } /></td>
-                                    </figure>
-                                  </Link>
-                                  {/*<td><img width="200" height="200" src={ movie.image } /></td>*/}
-                                  <td><h4>{ movie.type }</h4></td>
-                                  {/*<td><h4>{ movie.data.title }</h4></td>*/}
-
-                                  {/*<td><button onClick={() => {this.addComment(movie.id)} } className="button is-info">Comment</button></td>*/}
-                                </tr>
-                              ))}
-                              </tbody>
-                            </table>
-                           </div>
-                        </div>
-                      </div>
-                   </div>
-              </div>
-          </section>
-        </div>
-      )
-  }
-}
-export default ListPage;
+export default ListPage
