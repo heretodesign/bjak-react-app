312fa00a0267590ce296806e721e4b3e79b8e178
heretodesign <senwelo.works@gmail.com>
2019-08-23 12:36:38 +0800

    added react lazy to handle code spliting using react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.scss';
 import axios from 'axios'
 import { Route, Link } from "react-router-dom"
@@ -6,9 +6,10 @@ import Header from './components/layout/Header/Header.js'
 import Footer from './components/layout/Footer/Footer.js'
 import Landing from './components/content/Landing.js'
 import NoticeNav from './components/content/NoticeNav.js'
-import DetailPage from './pages/DetailPage.js'
-import ListPage from './pages/ListPage.js'
-import ViewPage from './pages/ViewPage.js'
+
+const ListPage = lazy(() => import('./pages/ListPage'));
+const ViewPage = lazy(() => import('./pages/ViewPage'));
+const DetailPage = lazy(() => import('./pages/DetailPage'));
 
 
 class App extends React.Component {
@@ -19,9 +20,11 @@ class App extends React.Component {
         <Header />
         <Landing />
         <NoticeNav />
-        <Route exact path="/" component={ListPage} />
-        <Route path="/pages/viewpage/:id" component={ViewPage} />
-        <Route path="/pages/detailpage/" component={DetailPage} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route exact path="/" component={ListPage} />
+          <Route path="/pages/viewpage/:id" component={ViewPage} />
+          <Route path="/pages/detailpage/" component={DetailPage} />
+        </Suspense>
         <Footer />
       </div>
     );
